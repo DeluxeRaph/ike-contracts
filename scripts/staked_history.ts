a7bd283fc1df26a845d12a4c6da64d01c0bfbed4
Brandon <brandon@kintsu.xyz>
2024-08-19 15:59:36 -0400

    track compounding events

--- scripts/staked_history.ts
+++ scripts/staked_history.ts
@@ -1,6 +1,7 @@
 import dotenv from "dotenv"
 import axios from "axios"
 import {initPolkadotJs} from "./utils/initPolkadotJs"
+import Compounds from "./Compounds.json" assert { type: 'json' }
 import Analytics from "./Analytics.json" assert { type: 'json' }
 
 const chainId = process.env.CHAIN || 'development'
@@ -8,109 +9,108 @@ dotenv.config({
   path: `.env.${chainId}`,
 })
 
+
+const BLOCKS = Compounds.map(c => c.block)
+BLOCKS.sort((a, b) => a - b)
+console.log(BLOCKS)
+
 async function main() {
   // Initialization
   const initParams = await initPolkadotJs()
   const {api} = initParams
 
-  const data = Analytics.data.analytics
-  const bonded = {
-    '69558413': {
-      '167': 10522124629456843n,
-      '168': 10171062575928059n,
-      '169': 800727936805088n,
-    },
-    '69558356': {
-      '167': 8387021223523283n,
-      '168': 0n,
-      '169': 11612978776476717n,
-    },
-    '69558331': {
-      '167': 13835073473640510n,
-      '168': 0n,
-      '169': 19164926526359490n,
-    },
-    '69558212': {
-      '167': 8381379799036568n,
-      '168': 0n,
-      '169': 11618620200963432n,
-    },
-    '69558070': {
-      '167': 10474061133419349n,
-      '168': 0n,
-      '169': 14525938866580151n,
-    },
+  const DB = []
+  let index_set = new Set<number>()
+
+  for (const BLOCK of BLOCKS) {
+    const index = Analytics.findIndex(a => a.block === BLOCK)
+    if (index >= 0) {
+      index_set.add(index - 1)
+      index_set.add(index)
+    }
   }
 
-  const table = []
-  const deltaTable = []
+  const indexes = Array.from(index_set)
+  indexes.sort((a, b) => a - b)
 
-  const MIN = 13_585
-  const MAX = 13_591
-  const INC = 1
+  console.log(indexes)
 
-  for (let i=Math.max(MIN, 0); i<Math.min(MAX, data.length); i+=INC) {
-    const block = data[i].block
-    const totalPooled = toBI(data[i].totalPooled)
+  for (const i of indexes) {
+    const block = Analytics[i].block
+    const totalPooled = toBI(Analytics[i].totalPooled)
 
-    console.log(`Looking up block hash for block ${block} (i=${i}) ...`)
     let blockHash = await getBlockHash(block) // await api.query.system.blockHash(blockNumber)
 
-    console.log(`Looking up points at block hash ${blockHash} ...`)
+    console.log(`Looking up data for block ${block} (i=${i}) (${blockHash}) ...`)
     const snapshotApi = await api.at(blockHash)
-    let points_167 = await snapshotApi.query.nominationPools.bondedPools(167).then(codec => codec.toHuman()['points']).then(toBI)
-    let points_168 = await snapshotApi.query.nominationPools.bondedPools(168).then(codec => codec.toHuman()['points']).then(toBI)
-    let points_169 = await snapshotApi.query.nominationPools.bondedPools(169).then(codec => codec.toHuman()['points']).then(toBI).catch(() => 0n)
-    const points_total = points_167 + points_168 + points_169
+    let nominationPools_points_167 = await snapshotApi.query.nominationPools.bondedPools(167).then(codec => codec.toHuman()['points']).then(toBI).catch(() => 0n)
+    let nominationPools_points_168 = await snapshotApi.query.nominationPools.bondedPools(168).then(codec => codec.toHuman()['points']).then(toBI).catch(() => 0n)
+    let nominationPools_points_169 = await snapshotApi.query.nominationPools.bondedPools(169).then(codec => codec.toHuman()['points']).then(toBI).catch(() => 0n)
+    const nominationPools_points = nominationPools_points_167 + nominationPools_points_168 + nominationPools_points_169
 
-    if (table.length > 0) {
-      const tableEntry = table[table.length - 1]
-      const pointsDelta_167 = points_167 - tableEntry.points_167
-      const pointsDelta_168 = points_168 - tableEntry.points_168
-      const pointsDelta_169 = points_169 - tableEntry.points_169
-      const bonded_167 = bonded[block]?.[167] ?? 0n
-      const bonded_168 = bonded[block]?.[168] ?? 0n
-      const bonded_169 = bonded[block]?.[169] ?? 0n
+    let [staking_total_167, staking_active_167] = await snapshotApi.query.staking.ledger('5EYCAe5ijiYfAXEth5DNsom62h3M85mpW93WcHeBo5jGb9y2').then(codec => [codec.toHuman()['total'], codec.toHuman()['active']]).then(x => x.map(toBI)).catch(() => [0n, 0n])
+    let [staking_total_168, staking_active_168] = await snapshotApi.query.staking.ledger('5EYCAe5ijiYfAXEth5DNwNcpyzpp8QvuTb2N7e26KYei8YvK').then(codec => [codec.toHuman()['total'], codec.toHuman()['active']]).then(x => x.map(toBI)).catch(() => [0n, 0n])
+    let [staking_total_169, staking_active_169] = await snapshotApi.query.staking.ledger('5EYCAe5ijiYfAXEth5DNzwUZwJcH8k5zR31DczPzr1a9fya7').then(codec => [codec.toHuman()['total'], codec.toHuman()['active']]).then(x => x.map(toBI)).catch(() => [0n, 0n])
+    let staking_active = staking_active_167 + staking_active_168 + staking_active_169
 
-      deltaTable.push({
-        'block': block,
-        // 'hash': blockHash,
-        '-': "increase",
-        'pool167': pointsDelta_167,
-        'pool168': pointsDelta_168,
-        'pool169': pointsDelta_169,
-        'total': pointsDelta_167 + pointsDelta_168 + pointsDelta_169,
-      })
-      deltaTable.push({
-        '-': "bonded",
-        'pool167': bonded_167,
-        'pool168': bonded_168,
-        'pool169': bonded_169,
-        'total': bonded_167 + bonded_168 + bonded_169,
-      })
-      deltaTable.push({
-        '-': "diff",
-        'pool167': bonded_167 - pointsDelta_167,
-        'pool168': bonded_168 - pointsDelta_168,
-        'pool169': bonded_169 - pointsDelta_169,
-        'total': (bonded_167 + bonded_168 + bonded_169) - (pointsDelta_167 + pointsDelta_168 + pointsDelta_169)
-      })
-    }
-
-    table.push({
-      i,
+    DB.push({
       block,
-      points_167,
-      points_168,
-      points_169,
-      points_total,
-      totalPooled: totalPooled,
-      delta: points_total - totalPooled,
+      blockHash,
+      nominationPools_points_167,
+      nominationPools_points_168,
+      nominationPools_points_169,
+      nominationPools_points,
+      staking_total_167,
+      staking_total_168,
+      staking_total_169,
+      staking_active_167,
+      staking_active_168,
+      staking_active_169,
+      staking_active,
+      totalPooled,
     })
   }
 
+  const table = []
+  for (const block of BLOCKS) {
+    const index = DB.findIndex(db => db.block === block)
+    console.log(`Creating table entry for block ${block} (i=${index})`)
+    const pointsDelta_167 = DB[index].nominationPools_points_167 - DB[index - 1].nominationPools_points_167
+    const pointsDelta_168 = DB[index].nominationPools_points_168 - DB[index - 1].nominationPools_points_168
+    const pointsDelta_169 = DB[index].nominationPools_points_169 - DB[index - 1].nominationPools_points_169
+    const activeDelta_167 = DB[index].staking_active_167 - DB[index - 1].staking_active_167
+    const activeDelta_168 = DB[index].staking_active_168 - DB[index - 1].staking_active_168
+    const activeDelta_169 = DB[index].staking_active_169 - DB[index - 1].staking_active_169
+    const totalPooledDelta = DB[index].totalPooled - DB[index - 1].totalPooled
+
+    table.push({
+      'block': block,
+      '-': "totalPooled",
+      'total': totalPooledDelta,
+    })
+    table.push({
+      '-': "nominationPools.points",
+      'pool167': pointsDelta_167,
+      'pool168': pointsDelta_168,
+      'pool169': pointsDelta_169,
+      'total': pointsDelta_167 + pointsDelta_168 + pointsDelta_169,
+    })
+    table.push({
+      '-': "staking.ledger.active",
+      'pool167': activeDelta_167,
+      'pool168': activeDelta_168,
+      'pool169': activeDelta_169,
+      'total': activeDelta_167 + activeDelta_168 + activeDelta_169,
+    })
+    table.push({
+      '-': "diff",
+      'pool167': activeDelta_167 - pointsDelta_167,
+      'pool168': activeDelta_168 - pointsDelta_168,
+      'pool169': activeDelta_169 - pointsDelta_169,
+      'total': (activeDelta_167 + activeDelta_168 + activeDelta_169) - (pointsDelta_167 + pointsDelta_168 + pointsDelta_169)
+    })
+  }
   console.table(table)
-  console.table(deltaTable)
 
   await api.disconnect()
 }
